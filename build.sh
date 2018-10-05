7819195b1a566b782322800cf466e8f97865989b
Alfonso Palacios <alpalacios@google.com>
2018-10-05 10:58:29 +0200

    Add env var to pass on the db connection string

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # get the decrypted secrets
 . secrets.sh
@@ -6,7 +6,7 @@
 # Update the Cloud SQL connection string and password in the app.yaml file
 APP_CONFIG=app.yaml
 sed -e "s/\[SECRET_CLOUDSQL_PASSWORD\]/${CLOUDSQL_PASSWORD}/" ${APP_CONFIG} > ${APP_CONFIG}_new && mv ${APP_CONFIG}_new ${APP_CONFIG}
-sed -e "s/\[CLOUDSQL_CONNECTION\]/${_DB_CONNECTION_STR}/" ${APP_CONFIG} > ${APP_CONFIG}_new && mv ${APP_CONFIG}_new ${APP_CONFIG}
+sed -e "s/\[CLOUDSQL_CONNECTION\]/${DB_CONNECTION_STR}/" ${APP_CONFIG} > ${APP_CONFIG}_new && mv ${APP_CONFIG}_new ${APP_CONFIG}
 
 # add the build's tag to the home page
 HOME_PAGE=index.php
